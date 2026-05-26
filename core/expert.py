373a2644f0e90a0cdfc632f70cb219b34176b32f
A Bilgehan SERCE <a.bilgehan.serce@gmail.com>
2026-05-26 12:15:08 +0300

    [DEV] Tweaked `Expert` PID parameters, added `target` argument to `getAction` to remove waypoint derivation logic

--- core/expert.py
+++ core/expert.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from core.config import WAYPOINTS, Action, Physics
+from core.config import Action, Physics
 from core.controller import PIDController
 
 """
@@ -9,7 +9,7 @@ Expert Controller for 2D Drone Positioning Simulator
 This class implements a PID-based expert controller to simulate a skilled human pilot 
 with some imperfections.
 
-It's used to collect expert data to train the GAIL agent.
+It's used to collect expert data to train the BehaviorClone model for warm starting PPO.
 
 ------------ Human-Like Flaws ------------
 - Deadzones: Small errors are ignored to simulate human control imperfections.
@@ -20,7 +20,7 @@ It's used to collect expert data to train the GAIL agent.
 class Expert:
     def __init__(self):
         # ------------------------ Outer Loop (Tilt & Thrust) ------------------------
-        self.yPID = PIDController(kP=3.5, kD=3.0, limit=400.0)
+        self.yPID = PIDController(kP=3.5, kD=2.0, limit=400.0)
         self.xPID = PIDController(kP=0.012, kD=0.04, limit=0.5)
 
         # ------------------------ Inner Loop (Desired Tilt to Motor Differential) ------------------------
@@ -30,11 +30,6 @@ class Expert:
         self._baseX = (0.012, 0.04, 0.0)
         self._baseY = (3.5, 3.0, 0.0)
         self._baseTheta = (200.0, 95.0, 0.0)
-
-        # Waypoints for the expert to follow.
-        self.waypoints = WAYPOINTS
-        self.currWaypointIdx = 0
-        self.waypointThresh = 10.0  # In Pixels
 
         # ------------------------ Human-Expert Flaws ------------------------
         # Deadzones to simulate imperfect human control.
@@ -94,20 +89,9 @@ class Expert:
 
         return
 
-    def getAction(self, state, dt):
+    def getAction(self, state, target, dt):
         x, y, vx, vy, theta, omega = state[:6]
-        targetX, targetY = self.waypoints[self.currWaypointIdx]
-
-        # Switch waypoint if reached
-        dist = np.hypot(x - targetX, y - targetY)
-
-        if dist < self.waypointThresh:
-            self.currWaypointIdx = (self.currWaypointIdx + 1) % len(self.waypoints)
-            targetX, targetY = self.waypoints[self.currWaypointIdx]
-
-            # Reset PID integrals when changing targets
-            self.yPID.integral = 0
-            self.xPID.integral = 0
+        targetX, targetY = target
 
         # ------------------------ Outer Loop ------------------------
         errY = y - targetY  # Positive if drone is below the target (needs more thrust)
@@ -149,7 +133,6 @@ class Expert:
         # Store last status for HUD display
         self.currTargetX = targetX
         self.currTargetY = targetY
-        self.currDist = dist
         self.currTotalThrust = totalThrust
         self.currMotorR = motorR
         self.currMotorL = motorL
