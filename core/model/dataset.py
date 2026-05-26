45f0601282154c0bcb5471ae0a4b28d4d857d981
A Bilgehan SERCE <a.bilgehan.serce@gmail.com>
2026-05-26 12:53:28 +0300

    [AI] Fixed `Critic` implementation and `Policy` compatibility, added "Sigmoid Jacobian correction" for entropy calculation

--- core/model/dataset.py
+++ core/model/dataset.py
@@ -22,22 +22,49 @@ class ExpertDataset(Dataset):
         
         x, y, vx, vy, theta, omega, windFx, windFy = stateRaw
         
-        normX = (x - Screen.WIDTH / 2) / (Screen.WIDTH / 2)
-        normY = (y - Screen.HEIGHT / 2) / (Screen.HEIGHT / 2)
-        normVx = np.clip(vx / 1000.0, -1.0, 1.0)
-        normVy = np.clip(vy / 1000.0, -1.0, 1.0)
-        normTheta = np.clip(theta / math.pi, -1.0, 1.0)
-        normOmega = np.clip(omega / 5.0, -1.0, 1.0)
-        normWindFx = np.clip(windFx / 300.0, -1.0, 1.0)
-        normWindFy = np.clip(windFy / 300.0, -1.0, 1.0)
+        normX = np.clip(
+            (x - Screen.WIDTH / 2) / (Screen.WIDTH / 2),
+            -1.0, 1.0
+        )
+        normY = np.clip(
+            (y - Screen.HEIGHT / 2) / (Screen.HEIGHT / 2),
+            -1.0, 1.0
+        )
+
+        normVx = np.clip(
+            (vx / 1500.0),
+            -1.0, 1.0
+        )
+        normVy = np.clip(
+            (vy / 1500.0),
+            -1.0, 1.0
+        )
+
+        normTheta = np.clip(
+            (theta / math.pi),
+            -1.0, 1.0
+        )
+        normOmega = np.clip(
+            (omega / 10.0),
+            -1.0, 1.0
+        )
+
+        normWindFx = np.clip(
+            (windFx / 25.0),
+            -1.0, 1.0
+        )
+        normWindFy = np.clip(
+            (windFy / 25.0),
+            -1.0, 1.0
+        )
 
         normState = np.array([
             normX, normY, normVx, normVy, 
             normTheta, normOmega, normWindFx, normWindFy
         ], dtype=np.float32)
 
-        tState = torch.tensor(normState, dtype=torch.float32)
-        tAction = torch.tensor(action, dtype=torch.float32)
+        tState = torch.as_tensor(normState, dtype=torch.float32)
+        tAction = torch.as_tensor(action, dtype=torch.float32)
 
         return tState, tAction
 
